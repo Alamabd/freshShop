60ceea4853a39945b8936613b0bd1b61bdc97493
Alamabd <alamabdillah0108@gmail.com>
2024-05-19 23:21:01 +0700

    feat(client): toggle open search CTR + K

--- client/src/components/Nav.tsx
+++ client/src/components/Nav.tsx
@@ -1,9 +1,18 @@
-import { useRef, useState } from "react"
+import { useEffect, useRef, useState } from "react"
 import Search from "./Search"
 
 function Nav() {
     const navLink = useRef<HTMLUListElement | null>(null)
     const [ openSearch, setOpenSearch ] = useState<boolean>(false)
+
+    useEffect(() => {
+          window.addEventListener('keydown', (event) => { 
+            if(event.ctrlKey && event.key === 'k') {
+                event.preventDefault()
+                setOpenSearch(true)
+            }            
+          })
+    }, [])
 
     return (
         <nav className="relative flex items-center justify-between">
@@ -25,7 +34,8 @@ function Nav() {
                 </li>
             </ul>
             <div className="flex gap-8">
-                <button onClick={() => setOpenSearch(true)}>
+                <button onClick={() => setOpenSearch(true)} className="px-3 py-1 rounded-full border flex gap-12 items-center max-sm:border-none max-sm:px-0">
+                    <h1 className="opacity-40 max-sm:hidden">CTR + K</h1>
                     <svg className="w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M18.031 16.6168L22.3137 20.8995L20.8995 22.3137L16.6168 18.031C15.0769 19.263 13.124 20 11 20C6.032 20 2 15.968 2 11C2 6.032 6.032 2 11 2C15.968 2 20 6.032 20 11C20 13.124 19.263 15.0769 18.031 16.6168ZM16.0247 15.8748C17.2475 14.6146 18 12.8956 18 11C18 7.1325 14.8675 4 11 4C7.1325 4 4 7.1325 4 11C4 14.8675 7.1325 18 11 18C12.8956 18 14.6146 17.2475 15.8748 16.0247L16.0247 15.8748Z"></path></svg>
                 </button>
                 <svg className="w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor"><path d="M12.001 4.52853C14.35 2.42 17.98 2.49 20.2426 4.75736C22.5053 7.02472 22.583 10.637 20.4786 12.993L11.9999 21.485L3.52138 12.993C1.41705 10.637 1.49571 7.01901 3.75736 4.75736C6.02157 2.49315 9.64519 2.41687 12.001 4.52853ZM18.827 6.1701C17.3279 4.66794 14.9076 4.60701 13.337 6.01687L12.0019 7.21524L10.6661 6.01781C9.09098 4.60597 6.67506 4.66808 5.17157 6.17157C3.68183 7.66131 3.60704 10.0473 4.97993 11.6232L11.9999 18.6543L19.0201 11.6232C20.3935 10.0467 20.319 7.66525 18.827 6.1701Z"></path></svg>
