04a5191b316516ac51f0cc19b702dd0f1e84496a
Alamabd <alamabdillah0108@gmail.com>
2024-05-19 21:33:52 +0700

    feat(client): crausel component

---
+++ client/src/components/Carausel.tsx
@@ -0,0 +1,29 @@
+import Slider from 'react-slick'
+import "slick-carousel/slick/slick.css"
+import "slick-carousel/slick/slick-theme.css"
+
+function Carausel() {
+    return(
+        <div className='sm:w-1/2 sm:mx-auto'>
+            <Slider {...{
+                dots: true,
+                infinite: true,
+                speed: 500,
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }}>
+                <div>
+                    <img src="./banner1.jpg" alt="banner1" />
+                </div>
+                <div>
+                    <img src="./banner2.jpg" alt="banner2" />
+                </div>
+                <div>
+                    <img src="./banner3.jpg" alt="banner3" />
+                </div>
+            </Slider>
+        </div>
+    )
+}
+
+export default Carausel
--- client/src/pages/Product.tsx
+++ client/src/pages/Product.tsx
@@ -1,3 +1,4 @@
+import Carausel from "../components/Carausel"
 import Header from "../components/Header"
 import Nav from "../components/Nav"
 import ProductList from "../components/ProductList"
@@ -7,12 +8,10 @@ function Product() {
     <div>
       <Header />
       <Nav />
-      <main className="mx-12 mt-8 flex gap-8 max-sm:flex-col">
+      <main className="mx-12 mt-8">
+        <Carausel />
         <div className="w-2/3">
           <ProductList />
-        </div>
-        <div className="w-1/3 max-sm:w-full">
-          <img src="./banner.jpg" alt="banner" />
         </div>
       </main>
     </div>
